a0de06055e7396bddffd40dbe829453ed70559bd
icukaegbu <icukaegbu@gmail.com>
2014-07-18 23:16:59 +0100

    Resolving Errors with Tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -10,21 +10,25 @@ var stringifyJSON = function(obj) {
   if (typeof obj === 'undefined' || typeof obj === 'function' ) return undefined;
   else if (typeof obj === 'string') return "\"" + obj + "\"";
   else if (typeof obj === 'boolean' || typeof obj === 'number') return obj.toString();
-  else if (obj === null) return "null";
+  else if (obj === null) return 'null';
 
   //else if(Array.isArray(obj) && obj.length > 0 ) {
   else if(Array.isArray(obj)) {
     //if empty array, return empty string
-    if (obj.length === 0) return "[]";
+    if (obj.length === 0) return '[]';
 
     accumulator = "[";
 
     for(var indx = 0; indx < obj.length; indx++){
       //if function, append null to the accumulator, else call recursively
-      accumulator += (typeof obj[indx] === 'function') ? "null," : stringifyJSON(obj[indx]) + ",";
+      accumulator += (typeof obj[indx] === 'function') ? 'null,' : stringifyJSON(obj[indx]) + ",";
     }
+    // _.each(obj, function(value){
+    //     accumulator += (typeof value === 'function') ? "null," : stringifyJSON(value) + ",";
+    // });
+
   	//eliminate the last comma; test if the last character in the string is a comma
-    if (accumulator.charAt(accumulator.length-1) === ",") accumulator = accumulator.slice(0, accumulator.length-2); //accumulator.slice(-1);/
+    if (accumulator.charAt(accumulator.length-1) === ",") accumulator = accumulator.slice(0, accumulator.length-2); 
     //if (accumulator.length > 1 && accumulator.charAt(accumulator.length-1) === ",") accumulator = accumulator.slice(0, accumulator.length-2); //accumulator.slice(-1);
 
     accumulator += "]";
@@ -34,7 +38,7 @@ var stringifyJSON = function(obj) {
 
   else{
     //if empty object, return empty string
-    if (Object.keys(obj).length === 0) return "{}";
+    if (Object.keys(obj).length === 0) return '{}';
 
     accumulator = "{";
 
@@ -46,7 +50,13 @@ var stringifyJSON = function(obj) {
       
     }
 
-    if (accumulator.charAt(accumulator.length-1) === ",") accumulator = accumulator.slice(0, accumulator.length-2); //accumulator.slice(-1);
+    // _.each(obj, function(value, key){
+    //   if (!(typeof value === 'function' || typeof value === 'undefined')) {
+    //     accumulator += "\""+key+"\":"+stringifyJSON(value)+",";
+    //   }
+    // });
+
+    if (accumulator.charAt(accumulator.length-1) === ",") accumulator = accumulator.slice(0, accumulator.length-2);
     //if (accumulator.length > 1 && accumulator.charAt(accumulator.length-1) === ",") accumulator = accumulator.slice(0, accumulator.length-2); //accumulator.slice(-1);
 
     accumulator += "}";
